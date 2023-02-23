674b11f7db5c5f200140d4947f0d1b2eba9fff37
LAICHEEHOONG <laicheehoong@gmail.com>
2023-02-23 22:24:08 +0800

    change React page text
    change React page text

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Deploy to heroku use CI/CD, GIT
         </a>
       </header>
     </div>
