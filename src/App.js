c4e5a5769fe31660c7c3c6fe0f5ca2572e271f22
LAICHEEHOONG <laicheehoong@gmail.com>
2023-02-23 22:39:41 +0800

    Try merge pr3
    Try merge pr3

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.fix bug
+          Edit <code>src/App.js</code> and save to reload.Merge PR-3
         </p>
         <a
           className="App-link"
